5ea608917e28126fefe381d0ea90867632bb7d7c
1831224 <1831224@LABCMAQ16.itp.local>
2019-11-12 12:24:42 -0300

    dadsafaf

---
+++ controlador/adm.controlador.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once 'modelo/produtoModelo.php';
+require_once 'modelo/categoriaModelo.php';
+
+
+function listaProdutoseQuantidadedoEstoque() {
+    $dados = array();
+    $dados["produtos"] = pegarTodosProdutodoEstoque();
+    exibir("produto/listar", $dados);
+}
+function listaProdutosCategoria() {
+    $dados = array();
+    $dados["produtos"] = pegarTodosProdutodoEstoque();
+    exibir("produto/listar", $dados);
+}
+function listaPedidosRealizadosDatas() {
+    $dados = array();
+    $dados["produtos"] = pegarTodosProdutodoEstoque();
+    exibir("produto/listar", $dados);
+}
+function listaPedidosMunicipioEstado() {
+    $dados = array();
+    $dados["produtos"] = pegarTodosProdutodoEstoque();
+    exibir("produto/listar", $dados);
+}
+function totalFaturamentoPeriodo() {
+    $dados = array();
+    $dados["produtos"] = pegarTodosProdutodoEstoque();
+    exibir("produto/listar", $dados);
+}
---
+++ modelo/adm.modelo.php
@@ -0,0 +1,11 @@
+<?php
+
+function pegarTodosProdutoEstoque() {
+    $sql = "";
+    $resultado = mysqli_query(conn(), $sql);
+    $produto = array();
+    while ($linha = mysqli_fetch_assoc($resultado)) {
+        $produto[] = $linha;
+    }
+    return $produto;
+}
